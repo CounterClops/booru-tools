fd3bab442d4be6ad67c21d308a1a92e31c4397cf
CounterClops <19741838+CounterClops@users.noreply.github.com>
2024-12-23 22:33:09 +0800

    feat: remove old manual sync code

--- booru_tools/__main__.py
+++ booru_tools/__main__.py
@@ -1,16 +1,3 @@
-# from booru_tools.core import BooruTools
-
-# booru_tools = BooruTools()
-# booru_tools.sync(
-#     urls=[
-#         "https://e621.net/posts/3052115",
-#         # "https://e621.net/pools/36579",
-#         # "https://e621.net/posts/1150307",
-#         # "https://e621.net/pools/36579",
-#         "https://e621.net/posts?tags=claweddrip+female+-male_on_male",
-#     ]
-# )
-
 import click
 from pathlib import Path
 
