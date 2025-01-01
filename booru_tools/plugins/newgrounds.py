4dd22aa665bfed5e32a309f84b5158fc8d3783dc
CounterClops <19741838+CounterClops@users.noreply.github.com>
2025-01-01 18:30:36 +0800

    feat: expand newgrounds metadata parsing

--- booru_tools/plugins/newgrounds.py
+++ booru_tools/plugins/newgrounds.py
@@ -1,7 +1,9 @@
+from loguru import logger
+from datetime import datetime
 import re
 
 from booru_tools.plugins import _plugin_template
-from booru_tools.shared import constants
+from booru_tools.shared import constants, resources
 
 class SharedAttributes:
     _DOMAINS = [
@@ -20,6 +22,13 @@ class SharedAttributes:
     
     POST_CATEGORY_MAP = {}
 
+    POST_SAFETY_MAPPING = {
+        "g": constants.Safety.SAFE,
+        "t": constants.Safety.SAFE,
+        "m": constants.Safety.SKETCHY,
+        "a": constants.Safety.UNSAFE
+    }
+
 class NewgroundsValidator(SharedAttributes, _plugin_template.ValidationPlugin):
     POST_URL_PATTERN = re.compile(r"(https:\/\/[w.]*newgrounds\.com\/portal\/view\/\d+)")
     USER_URL_PATTERN = re.compile(r"(https:\/\/(?!www\.)\w+\.newgrounds\.com)")
@@ -33,3 +42,45 @@ class NewgroundsValidator(SharedAttributes, _plugin_template.ValidationPlugin):
         if self.GLOBAL_URL_PATTERN.match(url):
             return constants.SourceTypes.GLOBAL
         return constants.SourceTypes._DEFAULT
+
+class NewgroundsMeta(SharedAttributes, _plugin_template.MetadataPlugin):
+    def get_id(self, metadata:dict) -> int:
+        id:int = metadata['index']
+        return id
+
+    def get_description(self, metadata:dict) -> str:
+        description:str = metadata.get("description", "")
+        return description
+    
+    def get_score(self, metadata:dict) -> int:
+        score:int = metadata.get("favorites", 0)
+        return score
+
+    def get_tags(self, metadata:dict[str, any]) -> list[resources.InternalTag]:
+        return []
+
+    def get_created_at(self, metadata:dict) -> datetime:
+        datetime_str:str = metadata["date"]
+        datetime_obj:datetime = datetime.fromisoformat(datetime_str)
+        return datetime_obj
+
+    def get_updated_at(self, metadata:dict) -> datetime:
+        return self.get_created_at(metadata=metadata)
+
+    def get_relations(self, metadata:dict) -> resources.InternalRelationship:
+        return resources.InternalRelationship()
+
+    def get_safety(self, metadata:dict) -> str:
+        rating:str = metadata["rating"]
+        safety:str = self.POST_SAFETY_MAPPING.get(rating, constants.Safety._DEFAULT)
+        return safety
+
+    def get_md5(self, metadata:dict) -> str:
+        return ""
+
+    def get_post_url(self, metadata:dict) -> str:
+        post_url:str = metadata['post_url']
+        return post_url
+
+    def get_pools(self, metadata:dict) -> list[resources.InternalPool]:
+        return []
