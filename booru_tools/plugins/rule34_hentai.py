4e392131f15ae25b0801808f1c6b9763872248cc
CounterClops <19741838+CounterClops@users.noreply.github.com>
2025-01-01 22:06:41 +0800

    feat: add rule34 alternative sites

--- booru_tools/plugins/rule34_hentai.py
+++ booru_tools/plugins/rule34_hentai.py
@@ -1,1 +1,32 @@
-# https://rule34hentai.net/
+import re
+
+from booru_tools.plugins import _plugin_template
+from booru_tools.shared import constants
+
+class SharedAttributes:
+    _DOMAINS = [
+        "rule34hentai.net"
+    ]
+    _CATEGORY = [
+        "rule34hentai"
+    ]
+    _NAME = "rule34hentai"
+
+    URL_BASE = "https://rule34hentai.net"
+
+    @property
+    def DEFAULT_POST_SEARCH_URL(self):
+        return f"{self.URL_BASE}/post/list"
+    
+    POST_CATEGORY_MAP = {}
+
+class Rule34HentaiValidator(SharedAttributes, _plugin_template.ValidationPlugin):
+    POST_URL_PATTERN = re.compile(r"(https:\/\/[a-zA-Z0-9.-]+\/post\/.+)|(https:\/\/[a-zA-Z0-9.-]+\/_images\/.*\/.*)")
+    GLOBAL_URL_PATTERN = re.compile(r"(https:\/\/[a-zA-Z0-9.-]+\/?$)")
+    
+    def get_source_type(self, url:str):
+        if self.POST_URL_PATTERN.match(url):
+            return constants.SourceTypes.POST
+        if self.GLOBAL_URL_PATTERN.match(url):
+            return constants.SourceTypes.GLOBAL
+        return constants.SourceTypes._DEFAULT
--- booru_tools/plugins/rule34_vault.py
+++ booru_tools/plugins/rule34_vault.py
@@ -1,1 +1,32 @@
-# https://rule34vault.com
+import re
+
+from booru_tools.plugins import _plugin_template
+from booru_tools.shared import constants
+
+class SharedAttributes:
+    _DOMAINS = [
+        "rule34vault.com"
+    ]
+    _CATEGORY = [
+        "rule34vault"
+    ]
+    _NAME = "rule34vault"
+
+    URL_BASE = "https://rule34vault.com"
+
+    @property
+    def DEFAULT_POST_SEARCH_URL(self):
+        return f"{self.URL_BASE}/"
+    
+    POST_CATEGORY_MAP = {}
+
+class Rule34VaultValidator(SharedAttributes, _plugin_template.ValidationPlugin):
+    POST_URL_PATTERN = re.compile(r"(https:\/\/[a-zA-Z0-9.-]+\/post\/.+)|(https:\/\/[a-zA-Z0-9.-]+\/posts\/[0-9]*\/[0-9]*\/.*)")
+    GLOBAL_URL_PATTERN = re.compile(r"(https:\/\/[a-zA-Z0-9.-]+\/?$)")
+    
+    def get_source_type(self, url:str):
+        if self.POST_URL_PATTERN.match(url):
+            return constants.SourceTypes.POST
+        if self.GLOBAL_URL_PATTERN.match(url):
+            return constants.SourceTypes.GLOBAL
+        return constants.SourceTypes._DEFAULT
