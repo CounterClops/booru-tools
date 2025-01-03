33e7baaeb6b9d95ce2f1041878d2a7bdffba39fe
CounterClops <19741838+CounterClops@users.noreply.github.com>
2025-01-04 00:22:17 +0800

    feat: add support for gelbooru

--- booru_tools/plugins/gelbooru.py
+++ booru_tools/plugins/gelbooru.py
@@ -1,5 +1,6 @@
 from booru_tools.plugins import _plugin_template
-from booru_tools.shared import errors, constants
+from booru_tools.shared import errors, constants, resources
+from datetime import datetime
 from loguru import logger
 import re
 
@@ -19,6 +20,76 @@ class SharedAttributes:
         return f"{self.URL_BASE}/index.php?page=post&s=list&tags="
     
     POST_CATEGORY_MAP = {}
+
+    POST_SAFETY_MAPPING = {
+        "general": constants.Safety.SAFE,
+        "sensitive": constants.Safety.SKETCHY,
+        "explicit": constants.Safety.UNSAFE
+    }
+
+class GelbooruMeta(SharedAttributes, _plugin_template.MetadataPlugin):
+    def __init__(self):
+        self.date_format = "%a %b %d %H:%M:%S %z %Y"
+        logger.debug(f"Loaded {self.__class__.__name__}")
+
+    def get_id(self, metadata:dict) -> int:
+        id:int = metadata['id']
+        return id
+
+    def get_sources(self, metadata:dict) -> list[str]:
+        source:int = metadata.get("source", "")
+        if not source:
+            sources:list = []
+        else:
+            sources:list = [source]
+        return sources
+
+    def get_description(self, metadata:dict) -> str:
+        return ""
+    
+    def get_score(self, metadata:dict) -> int:
+        score:int = metadata.get("score", 0)
+        return score
+
+    def get_tags(self, metadata:dict[str, any]) -> list[resources.InternalTag]:
+        all_tags:list[resources.InternalTag] = []
+        tags:str = metadata.get("tags", "")
+
+        for tag in tags.split(" "):
+            tag_resource = resources.InternalTag(
+                names=[tag]
+            )
+            all_tags.append(tag_resource)
+
+        return all_tags
+
+    def get_created_at(self, metadata:dict) -> datetime:
+        datetime_str:str = metadata["created_at"]
+        datetime_obj:datetime = datetime.strptime(datetime_str, self.date_format)
+        return datetime_obj
+
+    def get_updated_at(self, metadata:dict) -> datetime:
+        return self.get_created_at(metadata=metadata)
+
+    def get_relations(self, metadata:dict) -> resources.InternalRelationship:
+        return resources.InternalRelationship()
+
+    def get_safety(self, metadata:dict) -> str:
+        rating:str = metadata["rating"]
+        safety:str = self.POST_SAFETY_MAPPING.get(rating.lower(), constants.Safety._DEFAULT)
+        return safety
+
+    def get_md5(self, metadata:dict) -> str:
+        md5:str = metadata.get("md5", "")
+        return md5
+
+    def get_post_url(self, metadata:dict) -> str:
+        post_id = self.get_id(metadata=metadata)
+        url = f"{self.URL_BASE}/index.php?page=post&s=view&id={post_id}"
+        return url
+
+    def get_pools(self, metadata:dict) -> list[resources.InternalPool]:
+        return []
 
 class GelbooruValidator(SharedAttributes, _plugin_template.ValidationPlugin):
     POST_URL_PATTERN = re.compile(r"(https:\/\/[a-zA-Z0-9.-]+\/.+page=post.+)|(https:\/\/[a-zA-Z0-9.-]+\/+samples\/.+)")
