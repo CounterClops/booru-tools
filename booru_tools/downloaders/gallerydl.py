a736c6f7ee443fbc76ebd2a5cd65f21b43876b78
CounterClops <19741838+CounterClops@users.noreply.github.com>
2025-02-11 23:22:58 +0800

    feat: improve gallery_dl download continue checks

--- booru_tools/downloaders/gallerydl.py
+++ booru_tools/downloaders/gallerydl.py
@@ -7,15 +7,19 @@ from booru_tools.downloaders import _base
 from booru_tools.shared import constants, config
 
 class GalleryDlManager(_base.DownloadManager):
-    def __init__(self, extractor:str=None, page_size:int=50, extra_params:list=[]):
+    def __init__(self, extractor:str=None, extra_params:list=[]):
         logger.debug(f"Loading {self.__class__.__name__}")
         self.extractor:str = extractor
-        self.page_size:int = page_size
+        
         self.extra_params:list = extra_params
         self._downloaded_links:list[str] = []
 
-        config_manager = config.ConfigManager()
-        cookies_file = config_manager['networking']['cookies_file']
+        config_manager = config.shared_config_manager
+        cookies_file:Path = config_manager['networking']['cookies_file']
+        self.page_size:int = config_manager["downloaders"]["gallery_dl"]["page_size"]
+        self.allowed_blank_pages:int = config_manager["downloaders"]["gallery_dl"]["allowed_blank_pages"]
+        self.ignored_file_extensions:list[str] = config_manager["downloaders"]["gallery_dl"]["ignored_file_extensions"]
+
         if cookies_file:
             logger.debug(f"Using cookies file '{cookies_file}'")
             self.extra_params.extend([
@@ -60,25 +64,31 @@ class GalleryDlManager(_base.DownloadManager):
 
     def download_pending_items(self, job:_base.DownloadJob) -> _base.DownloadJob:
         urls = []
+
+        for item in job.download_items:
+            if item.resource.post_url:
+                item.download_url = item.resource.post_url
+            else:
+                logger.warning(f"Resource {item.resource} does not have a post_url, using {item._download_override} instead")
+                item.download_url = item._download_override
         
         for item in job.items_pending_download():
+            file_to_download = item.metadata_file.parent / item.metadata_file.stem
+            if file_to_download.suffix in self.ignored_file_extensions:
+                logger.debug(f"Skipping download of {file_to_download.name} as it's in the ignored file extensions list")
+                item.media_download_desired = False
+                continue
 
             if item.ignore:
                 continue
-
-            if item.resource.post_url:
-                download_url = item.resource.post_url
-            else:
-                logger.warning(f"Resource {item.resource} does not have a post_url, using {item._download_override} instead")
-                download_url = item._download_override
             
-            if download_url in self._downloaded_links:
-                logger.debug(f"Skipping download of {download_url} as it's already been downloaded")
+            if item.download_url in self._downloaded_links:
+                logger.debug(f"Skipping download of {item.download_url} as it's already been downloaded")
                 item.media_download_desired = False
                 continue
             
-            if download_url:
-                download_url = self.add_extractor_to_url(download_url)
+            if item.download_url:
+                download_url = self.add_extractor_to_url(item.download_url)
                 urls.append(download_url)
 
         if not urls:
@@ -124,10 +134,12 @@ class GalleryDlManager(_base.DownloadManager):
         min_range = 0
         max_range = self.page_size
 
-        downloaded_item_count = self.page_size
         self._downloaded_links:list[str] = []
+        self._sequential_blank_pages = 0
 
-        while downloaded_item_count:
+        continue_download = True
+
+        while continue_download:
             range = f"{min_range}-{max_range}"
             logger.debug(f"Downloading range {range} from {url}")
 
@@ -142,6 +154,38 @@ class GalleryDlManager(_base.DownloadManager):
             max_range += self.page_size
             
             yield job
-            downloaded_item_count = job.all_item_count(only_download_desired=True)
+            continue_download = self._check_continue_download(job)
         
         return
+    
+    def _check_continue_download(self, job:_base.DownloadJob) -> bool:
+        items_pending_download = job.items_pending_download()
+
+        new_items = [
+            item for item in job.download_items
+            if item.download_url not in self._downloaded_links
+        ]
+
+        new_items_found = bool(new_items)
+        if self.allowed_blank_pages == 0:
+            logger.debug(f"Allowed_blank_pages is set to 0, new items found equal {new_items_found}")
+            return new_items_found
+        
+        
+        logger.debug(f"Checking if download should continue")
+        if not new_items_found:
+            return False
+        
+        is_any_pending_downloads = bool(items_pending_download)
+        if not is_any_pending_downloads:
+            self._sequential_blank_pages += 1
+            logger.debug(f"Incrementing the sequential blank pages count to {self._sequential_blank_pages}")
+
+        is_under_allowed_pages = self._sequential_blank_pages < self.allowed_blank_pages
+        if is_under_allowed_pages:
+            continue_download = True
+        else:
+            logger.debug(f"Reached the blank page limit of {self.allowed_blank_pages}, stopping download")
+            continue_download = False
+            
+        return continue_download
