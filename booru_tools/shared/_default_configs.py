021cf349a427215912fb016e8cf1bd1fcb5d64cd
CounterClops <19741838+CounterClops@users.noreply.github.com>
2025-02-24 00:17:04 +0800

    feat: add extra default config options

--- booru_tools/shared/_default_configs.py
+++ booru_tools/shared/_default_configs.py
@@ -8,13 +8,14 @@ class DefaultConfigBaseGroup:
 ### Core
 @dataclass(kw_only=True)
 class DefaultCoreConfig(DefaultConfigBaseGroup):
-    blacklisted_tags:list = field(default_factory=list)
-    required_tags:list = field(default_factory=list)
-    allowed_safety:list = field(default_factory=lambda: ["safe", "sketchy", "unsafe"])
-    minimum_score:int = field(default=10)
-    destination:str = field(default="szurubooru")
-    add_video_metatags:bool = field(default=False)
-    cleanup_temp_directories:bool = field(default=True)
+    blacklisted_tags:list = field(default_factory=list, metadata={"description": "A list of blacklisted tags to avoid"})
+    required_tags:list = field(default_factory=list, metadata={"description": "A list of required tags to enforce"})
+    allowed_safety:list = field(default_factory=lambda: ["safe", "sketchy", "unsafe"], metadata={"description": "A list of the allowed post safety levels"})
+    minimum_score:int = field(default=10, metadata={"description": "The minimum score a post must have to be processed"})
+    destination:str = field(default="szurubooru", metadata={"description": "The destination plugin to use"})
+    add_video_metatags:bool = field(default=False, metadata={"description": "Whether to add video metadata tags to posts using ffmpeg"})
+    cleanup_temp_directories:bool = field(default=True, metadata={"description": "Whether to cleanup temporary directories after running"})
+    update_tag_categories:bool = field(default=True, metadata={"description": "Whether to update tag categories"})
 
 ### Commands
 @dataclass(kw_only=True)
@@ -68,6 +69,7 @@ class DefaultToolsFfmpegConfig(DefaultConfigBaseGroup):
     enabled:bool = field(default=True)
     create_duration_tags:bool = field(default=True)
     create_basic_video_tags:bool = field(default=True)
+    create_ffmpeg_processed_tag:bool = field(default=True)
 
 @dataclass(kw_only=True)
 class DefaultToolsConfig(DefaultConfigBaseGroup):
